1f11ffa05b007b988acc44edc38d1326c56568de
hiepNH272 <hiep.nh235708@sis.hust.edu.vn>
2025-05-09 12:00:16 +0700

    Main

---
+++ src/main/Collision.java
@@ -0,0 +1,30 @@
+package main;
+
+
+public class Collision {
+	
+	GamePanel gp;
+	
+	public Collision(GamePanel gp) {
+		this.gp = gp;
+	}
+	
+	public void checkTile(Character character) {
+
+	}
+	
+	public int checkObject (Character character, boolean player) { // check if entity co phai player hay k
+		int index = 999;
+		return index;
+	}
+	
+	public int checkOtherChar(Character character, Character[] target) {
+		int index = 999;
+
+		return index;
+	}
+	
+	public void checkOC_Player(Character character) {
+	}
+}
+
--- src/main/MouseHandler.java
+++ src/main/MouseHandler.java
@@ -2,49 +2,57 @@ package main;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class MouseHandler implements MouseListener {
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		GamePanel.player.isFiring = true;
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		GamePanel.player.isFiring = false;
-	}
+public class MouseHandler implements MouseListener, MouseMotionListener {
+	
+	public int x, y;
+	public boolean pressed;
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		x = e.getX();
+		y = e.getY();
+		
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
+		x = e.getX();
+		y = e.getY();
+		
+	}
 
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		pressed = true;
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		pressed = false;
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
